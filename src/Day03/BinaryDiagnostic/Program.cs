2ebf60cd7315c685c7b863040e6ba0446b6fb2c2
Jeremiah Altepeter <jeremiah.altepeter@healthgrades.com>
2021-12-03 10:11:39 -0700

    Finish Part 1

--- src/Day03/BinaryDiagnostic/Program.cs
+++ src/Day03/BinaryDiagnostic/Program.cs
@@ -1,14 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace BinaryDiagnostic
 {
     public static class Program
     {
+        private static int ToInt(this char c)
+        {
+            return c - '0';
+        }
+
         public static void Main(string[] args)
         {
             string[] lines = File.ReadAllLines("input.txt");
+
+            var results = ReadReport(lines);
+
+            var gamma = Convert.ToInt32(string.Join("", results.gamma), 2);
+            var epsilon = Convert.ToInt32(string.Join("", results.epsilon), 2);
+            
+            Console.WriteLine(gamma * epsilon);
+        }
+
+        public static (int[] gamma, int[] epsilon) ReadReport(IEnumerable<string> input)
+        {
+            var reports = input.Select(line => Array.ConvertAll(line.ToCharArray(), ToInt)).ToList();
+
+            var size = reports[0].Length;
+
+            var gamma = new int[size];
+            var epsilon = new int[size];
+
+            for (var i = 0; i < size; i++)
+            {
+                var bits = reports.Select(report => report[i]).ToList();
+
+                gamma[i] = bits.GroupBy(v => v).OrderByDescending(g => g.Count()).First().Key;
+                epsilon[i] = bits.GroupBy(v => v).OrderBy(g => g.Count()).First().Key;
+            }
+
+            return (gamma, epsilon);
         }
     }
 }
--- src/Day03/BinaryDiagnostic_Tests/UnitTest1.cs
+++ src/Day03/BinaryDiagnostic_Tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 using NUnit.Framework;
 
 namespace BinaryDiagnostic_Tests
@@ -7,9 +9,16 @@ namespace BinaryDiagnostic_Tests
     {
 
         [Test]
-        public void Test1()
+        [TestCase(new object[]{"00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"}, 
+            ExpectedResult = 198)]
+        public int Test1(params string[] input)
         {
-            Assert.Pass();
+            var results =  BinaryDiagnostic.Program.ReadReport(input);
+            
+            var gamma = Convert.ToInt32(string.Join("", results.gamma), 2);
+            var epsilon = Convert.ToInt32(string.Join("", results.epsilon), 2);
+
+            return gamma * epsilon;
         }
     }
 }
