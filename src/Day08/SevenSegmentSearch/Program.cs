e343fa76765671edb33f627f2d7c7d3b5e8db5fe
Jeremiah Altepeter <jeremiah.altepeter@healthgrades.com>
2021-12-08 09:25:16 -0700

    Create day 8 projects

---
+++ src/Day08/SevenSegmentSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace SevenSegmentSearch
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("input.txt");
+        }
+    }
+}
