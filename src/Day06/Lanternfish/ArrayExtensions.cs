33c51e609caf85f1d73ee80448f02287ef60bc4e
Jeremiah Altepeter <jeremiah.altepeter@healthgrades.com>
2021-12-07 17:28:35 -0700

    Make extension method generic

--- src/Day06/Lanternfish/ArrayExtensions.cs
+++ src/Day06/Lanternfish/ArrayExtensions.cs
@@ -2,10 +2,10 @@ namespace Lanternfish
 {
     public static class ArrayExtensions
     {
-        public static long[] ShiftLeft(this long[] array)
+        public static T[] ShiftLeft<T>(this T[] array)
         {
             var size = array.Length;
-            long[] newArray = new long[size];
+            T[] newArray = new T[size];
             for (var i = 0; i < size; i++) newArray[i] = array[(i + 1) % size];
 
             return newArray;
