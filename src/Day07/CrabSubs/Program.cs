f0f25b41ff21581fb1398ce3facb6a7117765e96
Jeremiah Altepeter <jeremiah.altepeter@healthgrades.com>
2021-12-07 17:36:27 -0700

    Crate Day 7 Project

---
+++ src/Day07/CrabSubs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+
+namespace CrabSubs
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("input.txt");
+        }
+    }
+}
---
+++ src/Day07/CrabSubs_Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CrabSubs
+{
+    public class CrabSubsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void PartOneTests()
+        {
+            Assert.Pass();
+        }
+    }
+}
