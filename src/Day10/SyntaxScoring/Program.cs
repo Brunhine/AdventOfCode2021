8c2dc24bd1d58cb33aec1f4f1dd3a15d1b0e4520
Jeremiah Altepeter <jeremiah.altepeter@healthgrades.com>
2021-12-14 16:39:32 -0700

    Solve Day 10 Part 2

--- src/Day10/SyntaxScoring/Program.cs
+++ src/Day10/SyntaxScoring/Program.cs
@@ -2,6 +2,15 @@
 
 var lines = File.ReadAllLines("input.txt");
 
-var score = lines.Sum(SyntaxParser.GetLineScore);
+var illegalCharScore = lines.Sum(SyntaxParser.GetIllegalLineScore);
 
-Console.WriteLine($"The score is: {score}");
+Console.WriteLine($"The score is: {illegalCharScore}");
+
+var autoCompleteScores =
+    (from line in lines
+        where !SyntaxParser.IsIllegalLine(line)
+        select SyntaxParser.GetAutoCompleteLineScore(line)).ToList();
+
+autoCompleteScores.Sort();
+
+Console.WriteLine($"The middle autocomplete score is: {autoCompleteScores[autoCompleteScores.Count/2]}");
--- src/Day10/SyntaxScoring_Tests/SyntaxScoringTests.cs
+++ src/Day10/SyntaxScoring_Tests/SyntaxScoringTests.cs
@@ -17,8 +17,23 @@ public class SyntaxScoringTests
     {
         var lines = File.ReadAllLines("test_input.txt");
 
-        var score = lines.Sum(SyntaxParser.GetLineScore);
+        var score = lines.Sum(SyntaxParser.GetIllegalLineScore);
 
         Assert.AreEqual(26397, score);
+    }
+
+    [Test]
+    public void Part2Tests()
+    {
+        var lines = File.ReadLines("test_input.txt");
+
+        var scores =
+            (from line in lines
+                where !SyntaxParser.IsIllegalLine(line)
+                select SyntaxParser.GetAutoCompleteLineScore(line)).ToList();
+
+        scores.Sort();
+
+        Assert.AreEqual(288957, scores[scores.Count / 2]);
     }
 }
