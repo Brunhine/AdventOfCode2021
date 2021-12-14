19815f6b7c6df29ee8422ba5e0ce03ddaa7daa01
Jeremiah Altepeter <jeremiah.altepeter@healthgrades.com>
2021-12-14 16:47:43 -0700

    Create Day 11 Projects

---
+++ src/Day11/DumboOctopus_Tests/DumboOctopusTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace DumboOctopus_Tests;
+
+public class DumboOctopusTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Part1Tests()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Part2Tests()
+    {
+        Assert.Pass();
+    }
+}
