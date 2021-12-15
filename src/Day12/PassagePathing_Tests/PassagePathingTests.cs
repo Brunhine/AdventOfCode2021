86ed7c594e3d1cdf73a2ad51199a7b6a26441a7f
Jeremiah Altepeter <jeremiah.altepeter@healthgrades.com>
2021-12-14 18:19:01 -0700

    Create Day 12 Projects

---
+++ src/Day12/PassagePathing_Tests/PassagePathingTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace PassagePathing_Tests;
+
+public class PassagePathingTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Part1Tests()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void Part2Tests()
+    {
+        Assert.Pass();
+    }
+}
