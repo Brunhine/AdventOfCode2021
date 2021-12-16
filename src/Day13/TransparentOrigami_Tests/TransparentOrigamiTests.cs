66100cc94ef23d14776b71c39e30b35085885dcd
Jeremiah Altepeter <jeremiah.altepeter@healthgrades.com>
2021-12-15 17:12:01 -0700

    Create Day 13 Project

---
+++ src/Day13/TransparentOrigami_Tests/TransparentOrigamiTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace TransparentOrigami_Tests;
+
+public class TransparentOrigamiTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Part1Tests()
+    {
+        Assert.Pass();
+    }
+    
+    [Test]
+    public void Part2Tests()
+    {
+        Assert.Pass();
+    }
+}
