b33b24a363b002f9988548d30c2db573bae8a1d4
Jeremiah Altepeter <jeremiah.altepeter@healthgrades.com>
2021-12-18 19:13:05 -0700

    CReate Day 16 Projects

---
+++ src/Day16/PacketDecoder_Tests/PacketDecoderTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace PacketDecoder_Tests;
+
+public class PacketDecoderTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Part1Tests()
+    {
+        Assert.Pass();
+    }
+
+
+    [Test]
+    public void Part2Tests()
+    {
+        Assert.Pass();
+    }
+}
